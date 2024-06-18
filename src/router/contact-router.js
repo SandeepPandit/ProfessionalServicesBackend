8f2bf30357faa59bb227883caab57f46993e7024
Sandeep Pandit <sandeep.pandit.messi.10@gmail.com>
2024-06-19 01:43:02 +0530

    Revert "update"
    This reverts commit b84ff57ec25db165b1cdce0330fbcba2291836c4.

--- src/router/contact-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactForm = require("../src/controllers/contact-controller");
-
-router.post("/contact", contactForm);
-
-module.exports = router;
--- src/router/service-router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const services = require("../src/controllers/service-controller");
-const router = express.Router();
-
-router.get("/service", services);
-
-module.exports = router;
